72e70e7db56e088b7a5b7c9c6c62df5aa2f3f403
ErfanKashef <129071476+ErfanKashef@users.noreply.github.com>
2024-09-14 19:16:57 +0330

    Feat/#24 base dashboard (#25)
    * Finishing part of the dashboard and adding some components from shadcn and adding some icons
    
    * add browse item in images tab

---
+++ packages/icons/src/components/serach.tsx
@@ -0,0 +1,25 @@
+import { IconProps } from "../types/types";
+const Serchinicon = (props: IconProps) => {
+  const { size = 24, color = "currentColor", ...resProps } = props;
+
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      {...resProps}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+      <path d="M21 21l-6 -6" />
+    </svg>
+  );
+};
+
+export default Serchinicon;
