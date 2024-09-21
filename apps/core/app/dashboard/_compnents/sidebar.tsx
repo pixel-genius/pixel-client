456f5e2ea2dea4e3cc2033b9f77bf0f50c0c9481
Aynaz Heydariafshar <aynaz@arianalabs.io>
2024-09-21 14:41:14 +0330

    FEAT : add iage on bottom of side bar

--- apps/core/app/dashboard/_compnents/sidebar.tsx
+++ apps/core/app/dashboard/_compnents/sidebar.tsx
@@ -1,4 +1,5 @@
 import PixelIcon from "@repo/icons/pxiel";
+import Image from "next/image";
 
 // icons
 import LayoutDashboard from "../../../../../packages/icons/src/components/layout-dashboard";
@@ -31,19 +32,29 @@ const DASHBOARD_SIDEBAR_ITEMS = [
 
 const SideBarDashboard = () => {
   return (
-    <div className="h-full basis-[355px] bg-[#181818] px-4 py-6">
-      <div className="flex items-center space-x-2 mb-8">
-        <PixelIcon size={46} color="currentColor" />
-        <h1 className="text-base font-bold sm:text-lg md:text-lg lg:text-xl">
-          PixelGenius
-        </h1>
-      </div>
-      {DASHBOARD_SIDEBAR_ITEMS.map((item) => (
-        <div className="flex transition-all items-center space-y-1 space-x-2 p-4 hover:bg-[#282828] cursor-pointer rounded-md">
-          <div>{item.icon}</div>
-          <h4>{item.title}</h4>
+    <div className="h-full flex flex-col justify-between basis-[355px] bg-[#181818]">
+      <div className="px-4 py-6">
+        <div className="flex items-center space-x-2 mb-8">
+          <PixelIcon size={46} color="currentColor" />
+          <h1 className="text-base font-bold sm:text-lg md:text-lg lg:text-xl">
+            PixelGenius
+          </h1>
         </div>
-      ))}
+        {DASHBOARD_SIDEBAR_ITEMS.map((item) => (
+          <div className="flex transition-all items-center space-y-1 space-x-2 p-4 hover:bg-[#282828] cursor-pointer rounded-md">
+            <div>{item.icon}</div>
+            <h4>{item.title}</h4>
+          </div>
+        ))}
+      </div>
+      <div>
+        <Image
+          src="images/clip-path-group.svg"
+          width={453}
+          height={300}
+          alt="Picture of space"
+        />
+      </div>
     </div>
   );
 };
