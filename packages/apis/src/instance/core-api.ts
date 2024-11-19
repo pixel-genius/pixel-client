9c64906da8fceac4fa39cbb2ce61bc39de4186f8
aynaz <91536085+aynazheydariafshar@users.noreply.github.com>
2024-11-19 18:26:08 +0330

    FEAT: add function for refresh token & build post api (#104)
    Co-authored-by: Aynaz Heydariafshar <aynaz@arianalabs.io>

--- packages/apis/src/instance/core-api.ts
+++ packages/apis/src/instance/core-api.ts
@@ -1,4 +1,10 @@
-import axios from "axios";
+import { COOKIES } from "@repo/ui/constant/cookie";
+import { postRefreshToken } from "../services/core/accounts/refresh/post/post-refresh-token";
+import axios, { AxiosError } from "axios";
+import Cookie from "js-cookie";
+
+let isRefreshing = false;
+let failedQueue: any[] = [];
 
 export const coreApi = axios.create({
   baseURL: "http://37.152.182.32/", // You can set your base URL here
@@ -7,19 +13,73 @@ export const coreApi = axios.create({
   },
 });
 
+const processQueue = (
+  error: AxiosError | null,
+  token: string | null = null,
+) => {
+  failedQueue.forEach((prom) => {
+    if (error) {
+      prom.reject(error);
+    } else {
+      prom.resolve(token);
+    }
+  });
+  failedQueue = [];
+};
+
+coreApi.interceptors.request.use((config) => {
+  const accessToken = Cookie.get(COOKIES.ACCESS_TOKEN);
+  if (accessToken && config.headers) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+  return config;
+});
+
 coreApi.interceptors.response.use(
   (response) => response,
-  (error) => {
-    const status = error.response ? error.response.status : null;
+  async (error) => {
+    const originalRequest = error.config;
 
-    // if (status === 401) {
-    //   router.push("/login");
-    // } else if (status === 403) {
-    //   alert(
-    //     "Access denied. You do not have permission to perform this action.",
-    //   );
-    //   router.push("/forbidden");
-    // }
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      if (isRefreshing) {
+        return new Promise((resolve, reject) => {
+          failedQueue.push({ resolve, reject });
+        })
+          .then((token) => {
+            originalRequest.headers.Authorization = `Bearer ${token}`;
+            return axios(originalRequest);
+          })
+          .catch((err) => {
+            return Promise.reject(err);
+          });
+      }
+
+      originalRequest._retry = true;
+      isRefreshing = true;
+
+      const refreshToken = Cookie.get(COOKIES.REFRESH_TOKEN);
+      if (!refreshToken) {
+        return Promise.reject(error);
+      }
+
+      try {
+        const { data } = await postRefreshToken({ refresh: refreshToken });
+        const newAccessToken = data?.access;
+
+        Cookie.set(COOKIES.ACCESS_TOKEN, newAccessToken);
+        coreApi.defaults.headers.Authorization = `Bearer ${newAccessToken}`;
+        processQueue(null, newAccessToken);
+
+        return coreApi(originalRequest);
+      } catch (refreshError: any) {
+        processQueue(refreshError, null);
+        Cookie.remove(COOKIES.ACCESS_TOKEN);
+        Cookie.remove(COOKIES.REFRESH_TOKEN);
+        return Promise.reject(refreshError);
+      } finally {
+        isRefreshing = false;
+      }
+    }
 
     return Promise.reject(error);
   },
