0006ffb30bb215244ee4aa60b779f533724e47ff
mrbadri <mrbadri.dev@gmail.com>
2024-12-03 10:49:32 -0800

    refactor: singup otp page to ppr rendering

--- apps/core/app/test/page.tsx
+++ apps/core/app/test/page.tsx
@@ -1,55 +1,26 @@
 "use client";
 
-import { useRive, useStateMachineInput } from "@rive-app/react-canvas";
-import { useEffect } from "react";
-import { motion, useMotionValue, useSpring } from "framer-motion";
+import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
-export default function App() {
-  const { rive, RiveComponent } = useRive({
-    src: "/clean_the_car.riv",
-    stateMachines: "Motion",
-    autoplay: true,
-  });
+// Add Suspense to the root of your app to avoid layout shifting
 
-  // const inputRive = useStateMachineInput(rive, "State Machine 1", "Scroll");
+function SearchBar() {
+  const searchParams = useSearchParams();
 
-  // // Motion value to track scroll progress
-  // const scrollY = useMotionValue(0);
-  // const smoothScrollY = useSpring(scrollY, {
-  //   stiffness: 50,
-  //   damping: 20,
-  // });
+  const search = searchParams.get("search");
 
-  // // Handle scroll event
-  // useEffect(() => {
-  //   const handleScroll = () => {
-  //     const scrollTop = window.scrollY;
-  //     const scrollHeight = document.body.scrollHeight - window.innerHeight;
-  //     const scrollPercentage = (scrollTop / scrollHeight) * 100;
-
-  //     scrollY.set(scrollPercentage); // Set raw scroll percentage
-  //   };
-
-  //   window.addEventListener("scroll", handleScroll);
-  //   return () => window.removeEventListener("scroll", handleScroll);
-  // }, [scrollY]);
-
-  // // Sync spring value with Rive state machine input
-  // useEffect(() => {
-  //   const unsubscribe = smoothScrollY.onChange((value) => {
-  //     if (inputRive) {
-  //       inputRive.value = value; // Update Rive input based on smooth scroll
-  //     }
-  //   });
-
-  //   return () => unsubscribe();
-  // }, [smoothScrollY, inputRive]);
-
-  return (
-    <div style={{ height: "200vh" }}>
-      <div className="inset-0 fixed">
-        <RiveComponent />
-      </div>
-    </div>
-  );
+  // URL -> `/dashboard?search=my-project`
+  // `search` -> 'my-project'
+  return <>Search: {search}</>;
 }
+
+const TestPage = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SearchBar />
+    </Suspense>
+  );
+};
+
+export default TestPage;
