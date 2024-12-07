a6928cdf7a698e875165f0aa89484e87c7b94b23
aynazheydariafshar <aynazhadriafshar@gmail.com>
2024-12-07 14:30:13 +0330

    FEAT : add sidebar ui on shadcn sidebar

--- apps/core/app/dashboard/layout.tsx
+++ apps/core/app/dashboard/layout.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import SideBarDashboard from "./_compnents/sidebar";
+import { SidebarProvider, SidebarTrigger } from "@repo/ui/components/sidebar";
+import { AppSidebar } from "./_compnents/app-sidebar";
 
 export default function DashboardLayout({
   children,
@@ -7,9 +9,16 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }): JSX.Element {
   return (
-    <div className="flex h-full">
-      <SideBarDashboard />
-      <div className="flex-1">{children}</div>
-    </div>
+    // <div className="flex h-full">
+    //   <SideBarDashboard />
+    //   <div className="flex-1">{children}</div>
+    // </div>
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
   );
 }
