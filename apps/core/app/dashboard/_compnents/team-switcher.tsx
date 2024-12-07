e259ec6f791ac9e1d4f96b204ebc96ee50ec4113
aynazheydariafshar <aynazhadriafshar@gmail.com>
2024-12-07 15:47:27 +0330

    FEAT : build component for haeder of sidebar

---
+++ apps/core/app/dashboard/_compnents/team-switcher.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+
+//ui
+import {
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@repo/ui/components/sidebar";
+//logo
+import PixelIcon from "@repo/icons/pxiel";
+
+export function TeamSwitcher({
+  teams,
+}: {
+  teams: {
+    name: string;
+    logo: React.ElementType;
+    plan: string;
+  }[];
+}) {
+  return (
+    <SidebarMenu>
+      <SidebarMenuItem>
+        <SidebarMenuButton
+          size="lg"
+          className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+        >
+          <div className="flex aspect-square size-9 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+            <PixelIcon size={46} color="currentColor" />
+          </div>
+          <div className="grid flex-1 text-left text-lg leading-tight">
+            <span className="truncate font-semibold">PixelGenius</span>
+          </div>
+        </SidebarMenuButton>
+      </SidebarMenuItem>
+    </SidebarMenu>
+  );
+}
