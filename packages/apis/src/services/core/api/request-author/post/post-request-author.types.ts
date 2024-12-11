469f8d55a4aea81a8dffa45a1fa98d40560d462e
ErfanKashef <129071476+ErfanKashef@users.noreply.github.com>
2024-12-11 17:22:07 +0330

    Updating the mockup based on GitHub.

---
+++ packages/apis/src/services/core/api/request-author/post/post-request-author.types.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+import { postRequestAuthorSchema } from "./post-request-author.schema";
+
+// Response
+export type PostRequestAuthorRequest = z.input<typeof postRequestAuthorSchema.request>;
+
+export type PostRequestAuthorRequestTransofrmed = z.infer<
+  typeof postRequestAuthorSchema.request
+>;
+
+// Request
+export type PostRequestAuthorResponse = z.input<typeof postRequestAuthorSchema.response>;
+
+export type PostRequestAuthorResponseTransformed = z.infer<
+  typeof postRequestAuthorSchema.response
+>;
