a2b4134fe42bfe7c37f7ed9ba61e3ccf0853e902
ErfanKashef <129071476+ErfanKashef@users.noreply.github.com>
2024-12-21 18:24:29 +0330

    Adding a “remove” functionality to the card component.

--- apps/core/app/(landing)/_components/addtoCard.tsx
+++ apps/core/app/(landing)/_components/addtoCard.tsx
@@ -6,13 +6,27 @@ import { Input } from "@repo/ui/components/input";
 import { Button } from "@repo/ui/components/button";
 import Securityicon from "@repo/icons/security";
 import { Label } from "@repo/ui/components/label";
+import { useState } from "react";
 
 const Addtocard = () => {
+  const [cards, setCards] = useState([
+    { id: 1, title: "Traveler - Travel Agency", price: "$15" },
+    { id: 2, title: "Explorer - Adventure Company", price: "$20" },
+    // می‌توانی کارت‌های دیگری اضافه کنی
+  ]);
+
+  const handleRemove = (id: number) => {
+    setCards(cards.filter((card) => card.id !== id));
+  };
+
   return (
     <div className="flex ">
       <div className="bg-gray-600 w-[411px] p-4">
-        <Card />
+        {cards.map((card) => (
+          <Card key={card.id} id={card.id} onRemove={handleRemove} />
+        ))}
       </div>
+
       <div className="bg-green-600 p-4 w-[522px]">
         <p className="text-xl font-medium pb-5">Checkout</p>
         <div className="flex justify-center gap-3 items-center bg-red-500 p-2 rounded-lg">
--- apps/core/app/(landing)/_components/card.tsx
+++ apps/core/app/(landing)/_components/card.tsx
@@ -1,12 +1,17 @@
 import Removeicon from "@repo/icons/remove";
 import Image from "next/image";
 
-const Card = () => {
+interface CardProps {
+  id: number;
+  onRemove: (id: number) => void;
+}
+
+const Card: React.FC<CardProps> = ({ id, onRemove }) => {
   return (
-    <div className="bg-gray-700  rounded-lg  overflow-hidden">
+    <div className="bg-gray-700 mb-2 rounded-lg overflow-hidden">
       <div className="flex">
         <div className="p-4 flex gap-2">
-          <div className="">
+          <div>
             <Image
               src={
                 "https://s3-alpha-sig.figma.com/img/ce79/f737/ea54458a8146c49352213b25ce57f7b6?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=qqR8nckdLaYWUmx~ux-Tr2lG85ngiw5DrwWsMWH5AboiB8bmgVFk3ABciKTo4iNrJyyiewFKHqDn2~ggpyk-zxXIWVA9ArjUxExTRKKMkWKqijujvceqMJT7ZStrUC1l4GgigHdTpKaKot0tj0COe0Wzgosy7Hrn~5O8BreZGmvNGtwHyxbQmtF~ykzzv5AWntDMA~k326WShEbbtjrtEsSOX4o74Sx-eW8yBXq334BP7Kupiy9O6uprHsBimZ1iQ554twGyJoTz~~Fa64Brvqp1uqaFv25MDlDG2K7IbrtCmCvlGQkGWdeIg~ZrlOm0KXW1En~AT2vQYOZApk0mjA__"
@@ -22,7 +27,10 @@ const Card = () => {
             <p className="text-lg font-bold">$15</p>
           </div>
         </div>
-        <div className=" w-9 flex items-center justify-center bg-error-900 ">
+        <div
+          className="w-9 flex items-center justify-center bg-error-900 cursor-pointer"
+          onClick={() => onRemove(id)}
+        >
           <Removeicon size={24} />
         </div>
       </div>
