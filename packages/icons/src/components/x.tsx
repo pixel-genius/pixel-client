aaf4b4efa544101bdbd3a60aa01e9776d10f61d8
mrbadri <mrbadri.dev@gmail.com>
2025-01-01 22:54:51 +0330

    fix: issue on svg

--- packages/icons/src/components/x.tsx
+++ packages/icons/src/components/x.tsx
@@ -10,9 +10,9 @@ const Xicon = (props: IconProps) => {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className="icon icon-tabler icons-tabler-outline icon-tabler-x"
       {...resProps}
     >
