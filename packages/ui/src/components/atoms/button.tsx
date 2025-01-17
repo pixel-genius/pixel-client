36f413bcb7e737b1ffa3148844033190e75ebf6d
erfan <erfan.moqadasii@gmail.com>
2025-01-17 16:01:01 +0330

    feat: add loading animation icon to button

--- packages/ui/src/components/atoms/button.tsx
+++ packages/ui/src/components/atoms/button.tsx
@@ -4,6 +4,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@repo/ui/lib/utils";
 import { IconProps } from "@repo/icons/types";
+import OrbitingDotsLoading from "./orbitingDotsLoading";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm transition-colors px-3 py-4 transition focus:ring focus:ring-primary-600  ",
