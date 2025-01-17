d49a2cbadae27610a587dd678c884efeefbf9c36
Pouria <pooriaka7@gmail.com>
2025-01-17 17:32:45 +0330

    Feat/#122 attachment component (#134)
    * feat: added attachmentLanding and attachment hook (not completed)
    
    * attacment admin added (not completed)
    
    * attachments mock functionality and attachment admin completed and added some animations
    
    * attachment thumbnail added and added some icons
    
    * some fixes in attachment
    
    * attachment theme fixed
    
    ---------
    
    Co-authored-by: Pouria Kazemirad <p.kazemirad@yarhis.ir>
    Co-authored-by: ErfanKashef <129071476+ErfanKashef@users.noreply.github.com>

--- packages/ui/src/components/molecules/attachment/attachmentLanding/attachmentItem/attachmentItem.tsx
+++ packages/ui/src/components/molecules/attachment/attachmentLanding/attachmentItem/attachmentItem.tsx
@@ -1,16 +1,17 @@
-import Image from "next/image";
 import { motion } from "framer-motion";
+import Image from "next/image";
+import { AttachmentItemProps } from "../../useAttachment";
+import Typography from "../../../../atoms/typography";
 import { Trash2, Loader } from "lucide-react";
 import { Button } from "../../../../atoms/button";
-import { FileState } from "../../useAttachment";
 
-interface AttachmentItemProps {
-  file: FileState;
-  handleRemove: (name: string) => void;
-}
-const AttachmentItem = ({ file, handleRemove }: AttachmentItemProps) => {
+const AttachmentItem = ({
+  file,
+  canDeleteFile,
+  handleRemove,
+}: AttachmentItemProps) => {
   const handleClick = () => {
-    handleRemove(file.name);
+    handleRemove(file.id || file.name);
   };
   return (
     <div
@@ -18,7 +19,7 @@ const AttachmentItem = ({ file, handleRemove }: AttachmentItemProps) => {
       className="rounded-lg w-[60px] h-[60px] bg-white relative flex justify-center items-center"
     >
       {file.loading ? (
-        <div className="z-10 flex justify-center items-center absolute top-0 left-0 w-full h-full bg-[#000] rounded-lg opacity-60">
+        <div className="z-10 flex justify-center items-center absolute top-0 left-0 w-full h-full bg-black rounded-lg opacity-60">
           <motion.span
             animate={{
               rotate: [0, 180, 360],
@@ -36,15 +37,16 @@ const AttachmentItem = ({ file, handleRemove }: AttachmentItemProps) => {
       {file.fileUrl ? (
         <Image className="rounded-lg" src={file.fileUrl} fill alt={file.name} />
       ) : (
-        <strong className="text-[#737373]">{file.type}</strong>
+        <Typography className="text-foreground">{file.type}</Typography>
       )}
-      <Button
-        onClick={handleClick}
-        disabled={file.loading}
-        className="p-0 flex justify-center items-center absolute top-0 bg-[#DC2626] right-0 w-[20px] h-[20px] rounded-sm hover:bg-[#DC2626]"
-      >
-        <Trash2 color="#fff" size={14} />
-      </Button>
+      {file.loading || (!canDeleteFile && file?.previousUploaded) ? null : (
+        <Button
+          onClick={handleClick}
+          className="p-0 flex justify-center items-center absolute top-0 bg-error right-0 w-[20px] h-[20px] rounded-sm hover:bg-error"
+        >
+          <Trash2 color="#fff" size={14} />
+        </Button>
+      )}
     </div>
   );
 };
--- packages/ui/src/components/molecules/attachment/useAttachment.ts
+++ packages/ui/src/components/molecules/attachment/useAttachment.ts
@@ -1,7 +1,7 @@
 "use client";
 
-import { UsePostFileUpload } from "@repo/apis/core/accounts/file-upload/post/use-post-file-upload";
-import { ChangeEvent, useEffect, useMemo, useRef, useState } from "react";
+import { UsePostUploadfile } from "@repo/apis/core/base/upload/post/use-post-uploadfile";
+import { ChangeEvent, useMemo, useRef, useState } from "react";
 import { toast } from "sonner";
 
 export interface AttachmentProps {
@@ -12,24 +12,54 @@ export interface AttachmentProps {
   onChange: (fileIds: number[] | []) => void;
   maxSize?: number; // in MB
   fileCategory: string;
+  canDeleteFile?: boolean;
+  value?: [];
+  disabled?: boolean;
+  error?: string;
 }
 export interface FileState {
+  id?: number;
   name: string;
   type: string;
   size: number;
   loading?: true;
   fileUrl?: string;
+  previousUploaded?: boolean;
+}
+export interface AttachmentItemProps {
+  file: FileState;
+  canDeleteFile: boolean;
+  handleRemove: (arg: string | number) => void;
 }
 
 const useAttachment = ({
   allowedTypes,
   maxSize = 10,
   fileCategory,
+  canDeleteFile,
   onChange,
 }: AttachmentProps) => {
   const inputFileRef = useRef<HTMLInputElement>(null);
   const [files, setFiles] = useState<FileState[]>([]);
-  const fileUploadMutation = UsePostFileUpload();
+  const fileUploadMutation = UsePostUploadfile({
+    onSuccess: (res) => {
+      const ids: number[] = [];
+      const data = res.data;
+      if (data.data.length) {
+        data.data.forEach((item) => {
+          ids.push(item.id);
+        });
+        setFiles((prev) =>
+          prev.map((file, index: number) => {
+            delete file.loading;
+            ids[index] ? (file.id = ids[index]) : null;
+            return file;
+          }),
+        );
+      }
+      onChange(ids);
+    },
+  });
 
   const allowedTypesText = useMemo(() => {
     let tempTxt: string = "";
@@ -37,82 +67,93 @@ const useAttachment = ({
       allowedTypes.forEach((type, index) => {
         tempTxt +=
           type.toUpperCase() +
-          (index === allowedTypes.length - 2 ? " or " : ", ");
+          (index === allowedTypes.length - 2
+            ? " or "
+            : index === allowedTypes.length - 1
+              ? ""
+              : ", ");
       });
     }
     return tempTxt;
   }, [allowedTypes]);
 
-  const handleRemove = (name: string) => {
-    setFiles((prev) => prev.filter((file) => file.name !== name));
+  const handleRemove = (arg: string | number) => {
+    setFiles((prev) =>
+      prev.filter(
+        (file) => file[typeof arg === "string" ? "name" : "id"] !== arg,
+      ),
+    );
   };
+  const getUploadFiles = (
+    Files: Array<File>,
+    e: ChangeEvent<HTMLInputElement> | React.DragEvent<HTMLDivElement>,
+  ) => {
+    let filesInput = Files;
+    const includesType = filesInput.every((file) =>
+      allowedTypes?.includes(
+        file.type.slice(file.type.lastIndexOf("/") + 1, file.type.length),
+      ),
+    );
+    if (includesType) {
+      const maxSizeBytes = maxSize * 1024 * 1024;
+      const validSize = filesInput.every((file) => file.size <= maxSizeBytes);
+      if (validSize) {
+        files.forEach((file) => {
+          if (filesInput.some((filesInput) => filesInput.name === file.name)) {
+            toast.warning(`Some Files already exists!`);
+            filesInput = filesInput.filter(
+              (fileInput) => fileInput.name !== file.name,
+            );
+          }
+        });
+        setFiles((prev) => {
+          filesInput.forEach((file) => {
+            if (!prev.some((prevFile) => prevFile.name === file.name)) {
+              const tempObj: FileState = {
+                name: file.name,
+                size: file.size,
+                type: file.type.slice(
+                  file.type.lastIndexOf("/") + 1,
+                  file.type.length,
+                ),
+                loading: true,
+              };
 
-  useEffect(() => {
-    if (fileUploadMutation.data?.data?.ids?.length) {
-      onChange(fileUploadMutation.data?.data.ids);
-      setFiles((prev) =>
-        prev.map((file) => {
-          delete file.loading;
-          return file;
-        }),
-      );
-    }
-  }, [fileUploadMutation.isSuccess]);
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    let filesInput = Array.from(e.target.files || []);
-    if (filesInput.length) {
-      const includesType = filesInput.every((file) =>
-        allowedTypes?.includes(
-          file.type.slice(file.type.lastIndexOf("/") + 1, file.type.length),
-        ),
-      );
-
-      if (includesType) {
-        const maxSizeBytes = maxSize * 1024 * 1024;
-        const validSize = filesInput.every((file) => file.size <= maxSizeBytes);
-        if (validSize) {
-          files.forEach((file) => {
-            if (
-              filesInput.some((filesInput) => filesInput.name === file.name)
-            ) {
-              toast.warning(`Some Files already exists!`);
-              filesInput = filesInput.filter(
-                (fileInput) => fileInput.name !== file.name,
-              );
+              console.log();
+              if (file.type.includes("image"))
+                tempObj.fileUrl = URL.createObjectURL(file);
+              prev.push(tempObj);
             }
           });
-          setFiles((prev) => {
-            filesInput.forEach((file) => {
-              if (!prev.some((prevFile) => prevFile.name === file.name)) {
-                const tempObj: FileState = {
-                  name: file.name,
-                  size: file.size,
-                  type: file.type.slice(
-                    file.type.lastIndexOf("/") + 1,
-                    file.type.length,
-                  ),
-                  loading: true,
-                };
-                if (file.type.includes("image"))
-                  tempObj.fileUrl = URL.createObjectURL(file);
-                prev.push(tempObj);
-              }
-            });
-            return [...prev];
-          });
-          if (filesInput.length)
-            fileUploadMutation.mutate({ fileCategory, file: filesInput });
-        } else {
-          e.preventDefault();
-          toast.warning(`File size exceeds ${maxSize}MB!`);
-        }
+          return [...prev];
+        });
+        if (filesInput.length) fileUploadMutation.mutate({ file: filesInput });
       } else {
         e.preventDefault();
-        toast.warning(`File type must be : ${allowedTypesText}`);
+        toast.warning(`File size exceeds ${maxSize}MB!`);
       }
+    } else {
+      e.preventDefault();
+      toast.warning(`File type must be : ${allowedTypesText}`);
     }
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const filesInput = Array.from(e.target.files || []);
+    getUploadFiles(filesInput, e);
     e.target.value = "";
+  };
+
+  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    const filesInput = Array.from(e.dataTransfer.files);
+    getUploadFiles(filesInput, e);
+  };
+
+  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    const target = e.target as HTMLDivElement;
+    target.style.cursor = "grabbing";
   };
 
   return {
@@ -121,6 +162,8 @@ const useAttachment = ({
     setFiles,
     handleChange,
     handleRemove,
+    handleDragOver,
+    handleDrop,
     allowedTypesText,
   };
 };
