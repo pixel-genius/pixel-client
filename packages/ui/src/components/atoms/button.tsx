1d2092a4d16471bc6d35cfe20db40cd0d18dfaac
ErfanKashef <129071476+ErfanKashef@users.noreply.github.com>
2025-01-17 11:54:17 +0330

    Fixing button imports and updating import paths.

--- packages/ui/src/components/atoms/button.tsx
+++ packages/ui/src/components/atoms/button.tsx
@@ -40,23 +40,22 @@ const statusColorHandler = ({
   variant?: "primary" | "secondary" | "tertiary" | null;
   statusColor?: ButtonStatusColor;
   disabled?: boolean;
-}) => {
+}): string | undefined => {
   if (disabled) return "";
 
   switch (variant) {
-    case "primary":
-      if (statusColor === "success") return "bg-success-500";
-      if (statusColor === "warning") return "bg-warning-500";
-      if (statusColor === "error") return "bg-error-500";
-      break;
     case "secondary":
     case "tertiary":
       if (statusColor === "success") return "text-success-500";
       if (statusColor === "warning") return "text-warning-500";
       if (statusColor === "error") return "text-error-500";
       break;
+    case "primary":
     default:
-      return "";
+      if (statusColor === "success") return "bg-success-500";
+      if (statusColor === "warning") return "bg-warning-500";
+      if (statusColor === "error") return "bg-error-500";
+      break;
   }
 };
 
