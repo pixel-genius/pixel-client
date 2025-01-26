5fe89ab020954546d11f3fea0d558649516831b2
p.kazemirad <p.kazemirad@yarhis.com>
2025-01-26 18:02:27 +0330

    feat: add api of get product list

---
+++ packages/apis/src/services/core/shop/products/get/use-get-shop-product-list.ts
@@ -0,0 +1,31 @@
+import {
+  ApiError,
+  ApiResponse,
+  UseQueryProps,
+  WithParams,
+} from "@repo/apis/types/api.types";
+import { useQuery } from "@tanstack/react-query";
+import { getShopProductList } from "./get-shop-product-list";
+import {
+  GetShopProductListRequest,
+  GetShopProductListResponseTransformed,
+} from "./get-shop-product-list.types";
+
+export type UseGetShopProductListProps = UseQueryProps<
+  ApiResponse<GetShopProductListResponseTransformed>,
+  WithParams<GetShopProductListRequest>
+>;
+
+export const getShopProductListQueryKey = () => ["getShopProductList"];
+
+export const UseGetShopProductList = (props: UseGetShopProductListProps) => {
+  const { params, ...resProps } = props;
+
+  const query = useQuery<ApiResponse<GetShopProductListResponseTransformed>, ApiError>({
+  queryKey: getShopProductListQueryKey(),
+  queryFn: () => getShopProductList(params),
+    ...resProps,
+  });
+
+  return query;
+};
