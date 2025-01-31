567f422efe615a463a001475d8dc475ae949459f
ErfanKashef <129071476+ErfanKashef@users.noreply.github.com>
2025-01-31 18:13:14 +0330

    Enhance add to card and change the color and use Typography

--- apps/core/app/(landing)/_components/card-cart-skeleton.tsx
+++ apps/core/app/(landing)/_components/card-cart-skeleton.tsx
@@ -1,12 +1,12 @@
 const CardSkeleton = () => {
   return (
-    <div className="bg-background p-4 flex gap-2 flex-1 rounded-lg">
+    <div className="bg-card p-4 flex gap-2 flex-1 rounded-lg">
       <div>
-        <div className="rounded-lg w-[112px] h-[80px] bg-card"></div>
+        <div className="rounded-2xl w-[140px] h-[80px] bg-background"></div>
       </div>
-      <div className="flex flex-col gap-2">
-        <div className="rounded-lg w-[166px] h-[18px] bg-card"></div>
-        <div className="rounded-lg w-[47px] h-[18px] bg-card"></div>
+      <div className="flex flex-col justify-evenly gap-1">
+        <div className="rounded-lg w-[166px] h-[20px] bg-background"></div>
+        <div className="rounded-lg w-[47px] h-[20px] bg-background"></div>
       </div>
     </div>
   );
