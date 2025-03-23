32bcd0a75710ee75ec07e92d4334cc098ea98da4
Pouria <pooriaka7@gmail.com>
2025-03-23 15:07:43 +0330

    feat: products list (#195)
    * feat: product list mock ui
    
    * feat: product list mock
    
    * feat: add api of get product list
    
    * feat: api of product list
    
    * change the gile name
    
    * refactor: change filenames from camel case to kebab case in products list folder
    
    * fix: change some props
    
    ---------
    
    Co-authored-by: Pouria Kazemirad <p.kazemirad@yarhis.ir>
    Co-authored-by: p.kazemirad <p.kazemirad@yarhis.com>
    Co-authored-by: ErfanKashef <129071476+ErfanKashef@users.noreply.github.com>

---
+++ packages/apis/src/services/core/shop/products/get/get-shop-product-list.ts
@@ -0,0 +1,32 @@
+import { coreApi } from "#instance/core-api";
+import path from "path";
+import { ApiResponse } from "@repo/apis/types/api.types";
+import { requestHandler } from "@repo/apis/utils/request-handler";
+import { getShopProductListSchema as schema } from "./get-shop-product-list.schema";
+import {
+  GetShopProductListRequest,
+  GetShopProductListResponse,
+  GetShopProductListResponseTransformed,
+} from "./get-shop-product-list.types";
+
+const getShopProductListURL = () => path.join("/shop/products/");
+
+export const getShopProductList = async (
+  props: GetShopProductListRequest,
+): Promise<ApiResponse<GetShopProductListResponseTransformed>> => {
+  const payloadParsed = schema.request.parse(props);
+  const URL = getShopProductListURL();
+
+  const response = await requestHandler(
+    () =>
+      coreApi.get<GetShopProductListResponse>(URL, { params: payloadParsed }),
+    schema.response._def.schema,
+    {
+      isMock: false,
+    },
+  );
+
+  const dataParsed = schema.response.parse(response.data);
+
+  return { ...response, data: dataParsed };
+};
