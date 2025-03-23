494d259f1a14cce168a3beba3ef3047e5138f347
ErfanKashef <129071476+ErfanKashef@users.noreply.github.com>
2025-03-23 16:21:44 +0330

    feat:handel file format section Co-authored-by: Mohammad Reza Badri <mrbadri@users.noreply.github.com>

---
+++ apps/core/app/dashboard/products/create/_components/file-formate-section.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import React from "react";
+import { FileFormatField } from "./file-formate-field";
+import Typography from "@repo/ui/components/typography";
+
+export const FileFormatSection: React.FC = ({}) => {
+  return (
+    <div>
+      <div >
+        <Typography variant={"label/lg"} className="text-foreground">
+          File Formats Included
+        </Typography>
+        <Typography variant={"label/sm"} className="text-muted-foreground">
+          Specify the file formats that customers can download after purchase.
+        </Typography>
+      </div>
+      <FileFormatField
+        onChange={(val) => {
+          console.log(val);
+        }}
+        options={[
+          {
+            id: 1,
+            name: "Figma",
+            icon: "figma",
+          },
+          {
+            id: 2,
+            name: "afterEffect",
+            icon: "afterEffect",
+          },
+        ]}
+      />
+    </div>
+  );
+};
---
+++ apps/core/app/test/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React from "react";
+import { FileFormatField } from "../dashboard/products/create/_components/file-formate-field";
+import { FileFormatSection } from "../dashboard/products/create/_components/file-formate-section";
+
+const Page = () => {
+  return <FileFormatSection />;
+};
+
+export default Page;
