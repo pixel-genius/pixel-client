dc34863a33ed006451b208ff14570fed570f99b1
saharrahmai <sahar.rahmati60@gmail.com>
2025-04-14 20:12:38 +0330

    add component versionInfo and add handleChange and state

---
+++ apps/core/app/dashboard/products/create/_components/version/versionInfo.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import Typography from "@repo/ui/components/typography";
+import { Switch } from "@repo/ui/components/switch";
+
+export interface VersionInfoType {
+    title:string, 
+    description:string, 
+    version:string, 
+    checked:boolean
+
+} 
+const versionInfo = (data:VersionInfoType) => {
+
+    const [checked, setChecked] = useState<{ checked: boolean }>({ checked: data.checked });
+
+  return (
+    <div className="border-b border-solid border-border flex flex-row gap-12 py-6">
+        <div>
+            <Typography variant={"label/lg"} className="font-bold">{data.title}</Typography>
+            <Typography variant={"label/lg"} className="font-normal">{data.version}</Typography>
+        </div>
+        <div>
+            <Typography variant={"label/lg"} className="font-bold">Version Nots</Typography>
+            <Typography variant={"label/sm"} className="font-normal">{data.description}</Typography>
+        </div>    
+        <div className="flex flex-row gap-2 items-center">
+            <Typography variant={"label/sm"} className="font-medium">Deactivate</Typography>
+            <Switch checked={checked.checked} onCheckedChange={(value) => setChecked((prev) => ({...prev, checked: value}))} />
+            <Typography variant={"label/sm"} className="font-medium">Active</Typography>
+        </div>
+    </div>
+  );
+};  
+
+export default versionInfo;
