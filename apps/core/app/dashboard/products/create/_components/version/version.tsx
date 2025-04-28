04e3be3256464f92b1decf7ab8fed277ecc9cb02
mrbadri <badri0000badri@gmail.com>
2025-04-28 10:19:34 -0700

    feat: upgrade to next 15

--- apps/core/app/dashboard/products/create/_components/version/version.tsx
+++ apps/core/app/dashboard/products/create/_components/version/version.tsx
@@ -1,26 +1,34 @@
-import Typography from "@repo/ui/components/typography";
+import { Typography } from "@repo/ui/components";
 import VersionInfo from "./versionInfo";
 
 const Version = () => {
-  return <>
-    <Typography variant={"heading/sm"} className="font-bold">Version History</Typography>
-    <div className="bg-card rounded-lg mt-4 px-4 pb-6">
+  return (
+    <>
+      <Typography variant={"heading/sm"} className="font-bold">
+        Version History
+      </Typography>
+      <div className="bg-card rounded-lg mt-4 px-4 pb-6">
         {[
-            { 
-                title: "version", 
-                description: "A freelance graphic and UI/UX designer. I specialise in Web Design , logo and brand development, motion graphics, and offer design services to business of all sizes ....", 
-                version: "1.2.3",
-                checked: true
-            },
-            { 
-                title: "vesrion", 
-                description: "A freelance graphic and UI/UX designer. I specialise in Web Design , logo and brand development, motion graphics, and offer design services to business of all sizes ....", 
-                version: "2.2.4",
-                checked: false
-            }
-        ].map(data => <VersionInfo {...data} />)}
-    </div>
-  </>;
-};  
+          {
+            title: "version",
+            description:
+              "A freelance graphic and UI/UX designer. I specialise in Web Design , logo and brand development, motion graphics, and offer design services to business of all sizes ....",
+            version: "1.2.3",
+            checked: true,
+          },
+          {
+            title: "vesrion",
+            description:
+              "A freelance graphic and UI/UX designer. I specialise in Web Design , logo and brand development, motion graphics, and offer design services to business of all sizes ....",
+            version: "2.2.4",
+            checked: false,
+          },
+        ].map((data) => (
+          <VersionInfo {...data} />
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default Version;
--- apps/core/app/dashboard/products/create/_components/version/versionInfo.tsx
+++ apps/core/app/dashboard/products/create/_components/version/versionInfo.tsx
@@ -1,35 +1,62 @@
 import { useState } from "react";
-import Typography from "@repo/ui/components/typography";
-import { Switch } from "@repo/ui/components/switch";
+import { Typography, Switch } from "@repo/ui/components";
 
 export interface VersionInfoType {
-    title:string, 
-    description:string, 
-    version:string, 
-    checked:boolean
-
-} 
-const VersionInfo = (data:VersionInfoType) => {
-
-    const [checked, setChecked] = useState<{ checked: boolean }>({ checked: data.checked });
+  title: string;
+  description: string;
+  version: string;
+  checked: boolean;
+}
+const VersionInfo = (data: VersionInfoType) => {
+  const [checked, setChecked] = useState<{ checked: boolean }>({
+    checked: data.checked,
+  });
 
   return (
-    <div className="border-b border-solid border-border flex flex-row gap-12 py-6" key={data.title}>
-        <div>
-            <Typography variant={"label/lg"} className="font-bold">{data.title}</Typography>
-            <Typography variant={"label/lg"} weight="normal" className="text-foreground">{data.version}</Typography>
-        </div>
-        <div>
-            <Typography variant={"label/lg"} className="font-bold">Version Nots</Typography>
-            <Typography variant="label/sm" weight="normal" className="text-foreground">{data.description}</Typography>
-        </div>    
-        <div className="flex flex-row gap-2 items-center">
-            <Typography variant={"label/sm"} className="font-medium">Deactivate</Typography>
-            <Switch checked={checked.checked} onCheckedChange={(value) => setChecked((prev) => ({...prev, checked: value}))} />
-            <Typography variant={"label/sm"} className="font-medium">Active</Typography>
-        </div>
+    <div
+      className="border-b border-solid border-border flex flex-row gap-12 py-6"
+      key={data.title}
+    >
+      <div>
+        <Typography variant={"label/lg"} className="font-bold">
+          {data.title}
+        </Typography>
+        <Typography
+          variant={"label/lg"}
+          weight="normal"
+          className="text-foreground"
+        >
+          {data.version}
+        </Typography>
+      </div>
+      <div>
+        <Typography variant={"label/lg"} className="font-bold">
+          Version Nots
+        </Typography>
+        <Typography
+          variant="label/sm"
+          weight="normal"
+          className="text-foreground"
+        >
+          {data.description}
+        </Typography>
+      </div>
+      <div className="flex flex-row gap-2 items-center">
+        <Typography variant={"label/sm"} className="font-medium">
+          Deactivate
+        </Typography>
+        <Switch
+          checked={checked.checked}
+          onCheckedChange={(value) =>
+            setChecked((prev) => ({ ...prev, checked: value }))
+          }
+        />
+        <Typography variant={"label/sm"} className="font-medium">
+          Active
+        </Typography>
+      </div>
     </div>
   );
-};  
+};
 
 export default VersionInfo;
