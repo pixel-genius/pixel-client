7ab3f9844439136fd67aa59af6bf730984581cf6
Ali Ebrahimi kashef <ali.kashef29@yahoo.com>
2025-05-16 17:03:53 +0330

    feat(accounts): add request author feature with form and result components
    Implement the "Become an Author" feature, including form submission, API integration, and result display. This includes:
    - Adding schema, types, and API service for the request author endpoint
    - Creating form and result components with UI updates
    - Integrating the API with the form for submission and handling response

--- apps/core/app/(landing)/become-auther/_components/auther-form.tsx
+++ apps/core/app/(landing)/become-auther/_components/auther-form.tsx
@@ -1,8 +1,8 @@
 "use client";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { postRequestAuthorSchema } from "@repo/apis/core/api/request-author/post/post-request-author.schema";
-import { PostRequestAuthorRequest } from "@repo/apis/core/api/request-author/post/post-request-author.types";
-import { UsePostRequestAuthor } from "@repo/apis/core/api/request-author/post/use-post-request-author";
+import { postBecomeAutherSchema } from "@repo/apis/core/accounts/request-author/post/post-become-auther.schema";
+import { PostBecomeAutherRequest } from "@repo/apis/core/accounts/request-author/post/post-become-auther.types";
+import { usePostBecomeAuther } from "@repo/apis/core/accounts/request-author/post/use-post-become-auther";
 import { Button, Typography } from "@repo/ui/components";
 import { Input } from "@repo/ui/components";
 import { Label } from "@repo/ui/components";
@@ -12,15 +12,13 @@ import { useForm } from "react-hook-form";
 import { AutherLayout } from "./auther-layout";
 import AutherResult from "./auther-result";
 import Image from "next/image";
-import  BecomeAnAuthorimage from "../../_assets/become-auther.svg"
-  
-
+import BecomeAnAuthorimage from "../../_assets/become-auther.svg";
 
 const AutherForm = () => {
   const [isSubmitted, setIsSubmitted] = useState(false);
 
-  const form = useForm<PostRequestAuthorRequest>({
-    resolver: zodResolver(postRequestAuthorSchema.request),
+  const form = useForm<PostBecomeAutherRequest>({
+    resolver: zodResolver(postBecomeAutherSchema.request),
   });
 
   const {
@@ -28,14 +26,15 @@ const AutherForm = () => {
     handleSubmit,
     formState: { errors },
   } = form;
-
-  const mutation = UsePostRequestAuthor({
+  console.log("errors", errors);
+  const mutation = usePostBecomeAuther({
     onSuccess: () => {
       setIsSubmitted(true);
     },
   });
 
-  const onSubmit = (data: PostRequestAuthorRequest) => {
+  const onSubmit = (data: PostBecomeAutherRequest) => {
+    console.log("data", data);
     mutation.mutate(data);
   };
 
@@ -45,102 +44,97 @@ const AutherForm = () => {
         <AutherResult /> // Show result form after submission
       ) : (
         <AutherLayout>
-
           <div className="pt-40  grid grid-cols-1 md:grid-cols-2 gap-10 pb-20">
-          
             <div>
-            <div className="pb-6">
-            <Typography variant="heading/md" weight="bold">
-              Become an Author
-            </Typography>
-            <Typography variant="paragraph/md" weight="normal" className="text-muted-foreground">
-              Join the talented community of designers at PixelGenius by
-              invitation or by submitting your application using the form below.
-            </Typography>
-          </div>
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="flex flex-col gap-4"
-          >
-            <div>
-              <Input
-                placeholder="exm@gmail.com"
-                type="email"
-                label="Email"
-                {...register("email")}
-                error={errors.email?.message}
-              />
-            </div>
-            <div className="flex gap-2 flex-col w-auto sm:flex-row">
-              <Input
-                placeholder="Pixel"
-                label="First Name"
-                {...register("firstName")}
-                error={errors.firstName?.message}
-              />
-              <Input
-                placeholder="Genius"
-                label="Last Name"
-                {...register("lastName")}
-                error={errors.lastName?.message}
-              />
-            </div>
-            <div>
-              <Input
-                placeholder="https://www.myportfolio.com"
-                label="Portfolio Link"
-                {...register("portfolioLink")}
-                error={errors.portfolioLink?.message}
-              />
-            </div>
-            <div>
-              <Input
-                placeholder="Choose your file"
-                type="file"
-                label="Upload Your CV and Portfolio"
-              />
-            </div>
-            <div>
-              <Label className="font-normal text-gray-400">
-                Additional Information
-              </Label>
-              <Textarea
-                placeholder="write a short message about yourself"
-                className=" bg-card resize-none p-3"
-              />
-            </div>
-            <div>
-              <Button
-                type="submit"
-             variant="primary"
-            className="w-full"
-                isLoading={mutation.isPending}
+              <div className="pb-6">
+                <Typography variant="heading/md" weight="bold">
+                  Become an Author
+                </Typography>
+                <Typography
+                  variant="paragraph/md"
+                  weight="normal"
+                  className="text-muted-foreground"
+                >
+                  Join the talented community of designers at PixelGenius by
+                  invitation or by submitting your application using the form
+                  below.
+                </Typography>
+              </div>
+              <form
+                onSubmit={handleSubmit(onSubmit)}
+                className="flex flex-col gap-4"
               >
-                Submit
-              </Button>
-            </div>
-          </form>
-
+                <div>
+                  <Input
+                    placeholder="exm@gmail.com"
+                    type="email"
+                    label="Email"
+                    {...register("email")}
+                    error={errors.email?.message}
+                  />
+                </div>
+                <div className="flex gap-2 flex-col w-auto sm:flex-row">
+                  <Input
+                    placeholder="Pixel"
+                    label="First Name"
+                    {...register("first_name")}
+                    error={errors.first_name?.message}
+                  />
+                  <Input
+                    placeholder="Genius"
+                    label="Last Name"
+                    {...register("last_name")}
+                    error={errors.last_name?.message}
+                  />
+                </div>
+                <div>
+                  <Input
+                    placeholder="https://www.myportfolio.com"
+                    label="Portfolio Link"
+                    {...register("profile_link")}
+                    error={errors.profile_link?.message}
+                  />
+                </div>
+                <div>
+                  <Input
+                    placeholder="Choose your file"
+                    type="file"
+                    label="Upload Your CV and Portfolio"
+                  />
+                </div>
+                <div>
+                  <Label className="font-normal text-gray-400">
+                    Additional Information
+                  </Label>
+                  <Textarea
+                    placeholder="write a short message about yourself"
+                    className=" bg-card resize-none p-3"
+                    {...register("info")}
+                    // error={errors.profile_link?.message}
+                  />
+                </div>
+                <div>
+                  <Button
+                    type="submit"
+                    variant="primary"
+                    className="w-full"
+                    isLoading={mutation.isPending}
+                  >
+                    Submit
+                  </Button>
+                </div>
+              </form>
             </div>
             <div className="self-center">
               <Image
-               className="w-[600px] h-[600px] object-cover"
+                className="w-[600px] h-[600px] object-cover hidden md:block"
                 width={500}
                 height={500}
-                
-                    
-                src={ BecomeAnAuthorimage}
+                src={BecomeAnAuthorimage}
                 alt="pixel-genius-logo"
               />
-            
-
             </div>
-          
-        
-       
-
           </div>
-         
         </AutherLayout>
       )}
     </div>
---
+++ packages/apis/src/services/core/accounts/request-author/post/post-become-auther.ts
@@ -0,0 +1,31 @@
+import { coreApi } from "#instance/core-api"; 
+import { ApiResponse } from "@repo/apis/types/api.types";
+import { requestHandler } from "@repo/apis/utils/request-handler";
+import path from "path";
+import { postBecomeAutherSchema as schema } from "./post-become-auther.schema";
+import {
+  PostBecomeAutherRequest,
+  PostBecomeAutherResponseTransformed,
+} from "./post-become-auther.types";
+
+export const postBecomeAutherURL = () => path.join("/api//accounts/request-author/");
+
+export const postBecomeAuther = async (
+  props?: PostBecomeAutherRequest,
+): Promise<ApiResponse<PostBecomeAutherResponseTransformed>> => {
+  const payloadParsed = schema.request.parse(props);
+
+  const URL = postBecomeAutherURL();
+
+  const response = await requestHandler(
+    () => coreApi.post(URL, payloadParsed),
+    schema.response._def.schema,
+    {
+      isMock: true,
+    }
+  );
+
+  const dataParsed = schema.response.parse(response.data);
+
+  return { ...response, data: dataParsed };
+};
