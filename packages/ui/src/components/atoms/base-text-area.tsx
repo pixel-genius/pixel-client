55b67ec4a9586a9a0f5a3c71b57b9a52b906b1f4
saharrahmai <sahar.rahmati60@gmail.com>
2025-05-24 00:11:45 +0330

    create base-text-area component

---
+++ packages/ui/src/components/atoms/base-text-area.tsx
@@ -0,0 +1,48 @@
+import * as React from "react";
+
+import { cn } from "@repo/ui/lib/utils";
+import { cva, type VariantProps } from "class-variance-authority";
+
+export interface BaseTextAreaProps
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "size">,
+    VariantProps<typeof baseTextAreaVariants> {
+  error?: boolean;
+}
+
+export const baseTextAreaVariants = cva(
+  "flex h-10 w-full bg-card rounded-md border border-border ring-offset-background file:border-0 file:bg-transparent  transition-shadow duration-500 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 text-sm file:text-sm placeholder:text-muted-foreground font-normal file:font-medium focus:ring-2 focus:ring-primary focus:ring-offset-2",
+  {
+    variants: {
+      size: {
+        sm: "px-4 py-2.5 h-9",
+        md: "px-4 py-2.5 h-[52px]",
+        lg: "px-4 py-4 h-14",
+      },
+      error: {
+        true: "ring-2 ring-error focus-visible:ring-error focus-visible:ring-offset-2",
+      },
+    },
+    defaultVariants: {
+      size: "md",
+    },
+  },
+);
+
+const BaseTaxtArea = React.forwardRef<HTMLInputElement, BaseTextAreaProps>(
+  (props, ref) => {
+    const { size, className, type, error, ...resProps } = props;
+
+    return (
+      <input
+        type={type}
+        className={cn(baseTextAreaVariants({ size, error, className }))}
+        ref={ref}
+        {...resProps}
+      />
+    );
+  },
+);
+
+BaseTaxtArea.displayName = "BaseTextArea";
+
+export { BaseTaxtArea };
